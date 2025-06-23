b56b2c1092ebec04469fe63e49c89d4572099467
Vigneshwaran-dev <learningvig@gmail.com>
2025-06-23 11:40:30 +0530

    Added AddTask Subparser

--- TaskMate.py
+++ TaskMate.py
@@ -5,4 +5,18 @@ Parser = argparse.ArgumentParser(prog="TaskMate",
                                  epilog="Thank You for Using TaskMate")
 SubParser = Parser.add_subparsers(title="Sub-Commands",help="Commands to perform operations")
 
+
+# === === Add Task group === === 
+
+AddParser = SubParser.add_parser("add")
+AddGroup = AddParser.add_argument_group()
+
+AddGroup.add_argument("name",help="Add Your Task Name")
+AddGroup.add_argument("-d","--description",help="Add Your Task Description")
+AddGroup.add_argument("-s","--status",help="Add Your Task Status")
+AddGroup.add_argument("-p","--priority",help="Add Your Task Priority")
+
+AddGroup.set_defaults(func=AddTask)
+
 args = Parser.parse_args()
+args.func(args)

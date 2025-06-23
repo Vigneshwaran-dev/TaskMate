5d81149c2ab6eab0c16c0f84512660f6587a2313
Vigneshwaran-dev <learningvig@gmail.com>
2025-06-23 11:35:40 +0530

    Added prog,decs,epilog

--- TaskMate.py
+++ TaskMate.py
@@ -1,6 +1,8 @@
 import argparse
 
-Parser = argparse.ArgumentParser()
+Parser = argparse.ArgumentParser(prog="TaskMate",
+                                 description="Manage Your Everyday Tasks with ease",
+                                 epilog="Thank You for Using TaskMate")
 SubParser = Parser.add_subparsers()
 
 args = Parser.parse_args()
